989ee26bbf9b207f265c74b5a3fe2c8a3024f867
Daniel Schmidt <dsci@code79.net>
2011-05-27 12:03:39 +0200

    added string support

--- lib/mass_deletable.rb
+++ lib/mass_deletable.rb
@@ -16,11 +16,20 @@ module MassDeletable
     def mass_destroy(opt={})
       raise MassDeletable::Errors::MassDeletableHashError, "No bunch of items to delete given!" if opt.empty? 
       destroyed = true
+      if opt[:bunch].is_a?(String) and opt[:bunch].include?(",")
+        bunches = opt[:bunch].split(",")
+        bunches.each_with_index do |id,index|
+          bunches[index] = id.to_i
+        end
+        opt[:bunch] = bunches
+      end
       opt[:bunch].each do |item|
         if(item.kind_of?(ActiveRecord::Base))
           single_out_id = item.send(:id)
         else
-          raise MassDeletable::Errors::MassDeletableNotFixnumError, "Not support type of class!" unless item.kind_of?(Fixnum)
+          unless item.kind_of?(Fixnum)
+            item = item.to_i
+          end
           single_out_id = item
         end
         unless self.find(single_out_id).destroy
